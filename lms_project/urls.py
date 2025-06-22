3e7a92593fb085432442ca2269177cc0bb354053
Ashmin Dhungana <77155700+AshminDhungana@users.noreply.github.com>
2025-06-22 08:58:50 +0000

    Initial commit of Enhanced LMS Django project

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -1,9 +1,11 @@
 # lms_project/urls.py
 
 from django.contrib import admin
-from django.urls import path, include # Import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # Include URLs from the 'core' app
+    path('', include('core.urls')),
+    # Ensure Django's built-in authentication URLs are included under the 'accounts' namespace
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
